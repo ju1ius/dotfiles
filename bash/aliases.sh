34c12a2c6865232beada8167c3b66375d3dc00ca
ju1ius <jules.bernable@gmail.com>
2020-10-05 17:30:24 +0200

    adds aliases for docker-compose

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -11,7 +11,12 @@ alias gl="git log --oneline --decorate --graph"
 # https://gist.github.com/RichardBronosky/9af3b9796d7423cd0e8e1d419fcea9fc
 alias git-serve="git daemon --verbose --export-all --base-path=.git --reuseaddr --strict-paths .git/"
 
-alias dcmp="docker-compose"
+# docker-compose
+alias dc="docker-compose"
+alias dcx="docker-compose exec"
+alias dcu="docker-compose up -d"
+alias dcd="docker-compose down"
+alias dcl="docker-compose logs"
 
 # Enable XDebug from command-line
 alias php-debug="php -e -d zend_extension=xdebug.so"
