2090be7f8a80389adf9b58c76f7dc6e8a86b03f7
ju1ius <jules.bernable@gmail.com>
2022-01-09 15:12:06 +0100

    enables function & class textobjects

--- neovim/lua/my/plugins/treesitter.lua
+++ neovim/lua/my/plugins/treesitter.lua
@@ -13,7 +13,7 @@ configs.setup({
     enable = true,
     -- list of language that will be disabled
     disable = {''},
-    additional_vim_regex_highlighting = true,
+    additional_vim_regex_highlighting = false,
   },
   indent = {
     enable = true,
@@ -38,7 +38,22 @@ configs.setup({
     enable = true,
     -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring#commentnvim
     enable_autocmd = false,
-  }
+  },
+  -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  textobjects = {
+    select = {
+      enable = true,
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+  },
 })
 
 -- set foldmethod to expression
